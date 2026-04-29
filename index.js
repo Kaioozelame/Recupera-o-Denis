62a29c818bc8357a8c360879e0a7fb7e2effb996
Kaioozelame <kaio.ozelame.santos@escola.pr.gov.br>
2026-04-29 11:19:09 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+{
+  "expo": {
+    "name": "trabalho-mobile-1-tri",
+    "slug": "snack-50a05f3c-42c1-4284-b9ec-98595768b66f",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "userInterfaceStyle": "light",
+    "newArchEnabled": true,
+    "splash": {
+      "image": "./assets/splash-icon.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/adaptive-icon.png",
+        "backgroundColor": "#ffffff"
+      },
+      "edgeToEdgeEnabled": true
+    },
+    "web": {
+      "favicon": "./assets/favicon.png"
+    }
+  }
+}
